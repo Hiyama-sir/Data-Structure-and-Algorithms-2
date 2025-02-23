05c388aa71d41d467b0a9d976577db9f7c19b4ff
Hiyama <bn23257@shibaura-it.ac.jp>
2025-02-24 07:55:12 +0900

    アルゴリズムと実装の基礎3

---
+++ classの基礎/原点からの距離.py
@@ -0,0 +1,13 @@
+from math import sqrt
+
+class Cood:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+    
+    def calc_distance(self):
+        return sqrt(self.x**2 + self.y**2)
+
+obj = Cood(300,400)
+dist = obj.calc_distance()
+print(dist)
